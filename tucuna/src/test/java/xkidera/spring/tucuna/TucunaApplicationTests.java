6a920ff42aa608ce0a5847d69f00e0f1d076ad66
Felipe Souto Pereira <37711746+xkidera@users.noreply.github.com>
2021-07-10 19:21:52 -0300

    Add files via upload

---
+++ tucuna/src/test/java/xkidera/spring/tucuna/TucunaApplicationTests.java
@@ -0,0 +1,13 @@
+package xkidera.spring.tucuna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TucunaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
